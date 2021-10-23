3f04c561761705b7d0710baf3a21312466fad66e
seysay <makhniukserhii@gmail.com>
2021-10-23 18:24:38 +0300

    add psql, add style css

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,6 +13,6 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('main/default/index.html.twig', []);
+        return $this->render('main/default/index.html.twig');
     }
 }
