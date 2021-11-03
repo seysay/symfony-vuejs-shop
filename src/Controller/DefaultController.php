d42c61e082a944866462cfadcf6b182c510f9bfb
seysay <makhniukserhii@gmail.com>
2021-11-03 21:39:43 +0200

    add func

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\EditProductFormType;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", methods="GET", name="main_homepage")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('main/default/index.html.twig');
+        $entityManager = $this->getDoctrine()->getManager();
+        $productList = $entityManager->getRepository(Product::class)->findAll();
+
+        return $this->render('main/default/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/edit-product/{id}", methods="GET|POST", name="product_edit", requirements={"id"="\d+"})
+     * @Route("/add-product", methods="GET|POST", name="product_add")
+     */
+    public function editProduct(Request $request, int $id = null): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        if ($id) {
+            $product = $entityManager->getRepository(Product::class)->find($id);
+        } else {
+            $product = new Product();
+        }
+        $form = $this->createForm(EditProductFormType::class, $product);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('product_edit', ['id' => $product->getId()]);
+        }
+
+        return $this->render('main/default/edit_product.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
